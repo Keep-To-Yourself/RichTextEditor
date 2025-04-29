7ec8c286be601b980700201f4ea3e45a2deaf556
Dylan Deng <ballade0d@gmail.com>
2025-04-29 21:47:25 +0800

    Initial commit

---
+++ Example/RichTextEditor/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  RichTextEditor
+//
+//  Created by 30594690 on 04/29/2025.
+//  Copyright (c) 2025 30594690. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
